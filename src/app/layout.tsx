ef4fa384017c5a2a98066b57fae74ef0bfc85bea
parallax-kal <gthecoderkalisaineza@gmail.com>
2024-12-21 01:02:44 +0200

    [update] added favicon and working on navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/common/app-sidebar";
+import AppNavbar from "@/components/common/app-navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <SidebarProvider open={false}>
           <AppSidebar />
           <main>
+            <AppNavbar />
             {children}
           </main>
         </SidebarProvider>
